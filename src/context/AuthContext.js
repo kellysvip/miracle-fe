67db0dd6c686aa8eb622977ac01298917197a86d
Kelly <20522051@gm.uit.edu.vn>
2022-09-20 16:58:44 +0700

    register and persistent login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -23,11 +23,25 @@ const reducer = (state, action) => {
         isAuthenticated: true,
         user: action.payload.user,
       };
-      case REGISTER_SUCCESS:
+    case REGISTER_SUCCESS:
       return {
         ...state,
         isAuthenticated: true,
         user: action.payload.user,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    case INITIALIZE:
+      const { isAuthenticated, user } = action.payload;
+      return {
+        ...state,
+        isInitialized: true,
+        isAuthenticated,
+        user,
       };
 
     default:
@@ -49,6 +63,31 @@ const setSession = (accessToken) => {
 
 export function AuthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const accessToken = window.localStorage.getItem("accessToken");
+        if (accessToken && isValidToken(accessToken)) {
+          setSession(accessToken);
+          const res = await apiService.get("/users/me");
+          const user = res.data;
+          dispatch({
+            type: INITIALIZE,
+            payload: { isAuthenticated: true, user },
+          });
+        }
+      } catch (error) {
+        setSession(null);
+
+        dispatch({
+          type: INITIALIZE,
+          payload: { isAuthenticated: false, user: null },
+        });
+      }
+    };
+    initialize();
+  }, []);
+
   const login = async ({ email, password }, callback) => {
     const res = await apiService.post("/auth/login", { email, password });
     const { user, accessToken } = res.data;
@@ -59,8 +98,8 @@ export function AuthProvider({ children }) {
     });
     callback();
   };
-  const register = async ({name, email, password }, callback) => {
-    const res = await apiService.post("/users", {name, email, password });
+  const register = async ({ name, email, password }, callback) => {
+    const res = await apiService.post("/users", { name, email, password });
     const { user, accessToken } = res.data;
     setSession(accessToken);
     dispatch({
@@ -70,8 +109,16 @@ export function AuthProvider({ children }) {
     callback();
   };
 
+  const logout = async (callback) => {
+    setSession(null);
+    dispatch({
+      type: LOGOUT,
+    });
+    callback();
+  };
+
   return (
-    <AuthContext.Provider value={{ ...state, login, register }}>
+    <AuthContext.Provider value={{ ...state, login, register, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,133 @@
-import React from 'react'
+import { yupResolver } from "@hookform/resolvers/yup";
+import React, { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import * as Yup from "yup";
+import { FTextField } from "../components/form";
+import { FormProvider } from "../components/form";
+import { Link, useNavigate } from "react-router-dom";
+import { Container, Stack } from "@mui/system";
+import { Alert, IconButton, InputAdornment } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useForm } from "react-hook-form";
+import { LoadingButton } from "@mui/lab";
+const registerSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+  passwordConfirmation: Yup.string()
+    .required("Please confirm your password")
+    .oneOf([Yup.ref("password")], "Password must match"),
+});
+
+const defaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  passwordConfirmation: "",
+};
 
 const RegisterPage = () => {
-  return (
-    <div>
-      <h1>RegisterPage</h1>
-    </div>
-  )
-}
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] =
+    useState(false);
+  const methods = useForm({
+    resolver: yupResolver(registerSchema),
+    defaultValues,
+  });
+  const {
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors, isSubmitting },
+  } = methods;
 
-export default RegisterPage
+  const onSubmit = async (data) => {
+    let { name, email, password, passwordConfirmation } = data;
+    
+    try {
+     await auth.register({ name, email, password, passwordConfirmation }, () => {
+        navigate("/", { replace: true });
+      });
+    } catch (error) {
+      reset();
+      console.log(error);
+      setError("responseError", error);
+    }
+  };
+  return (
+    <Container maxWidth="xs">
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          {!!errors.responseError && (
+            <Alert severity="error">{errors.responseError.message}</Alert>
+          )}
+          <Alert severity="info">
+            You already have an account?{" "}
+            <Link variant="subtitle2" to="/login">
+              Sign in
+            </Link>
+          </Alert>
+
+          <FTextField name="name" label="Full name" />
+          <FTextField name="email" label="Email address" />
+
+          <FTextField
+            name="password"
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword(!showPassword)}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <FTextField
+            name="passwordConfirmation"
+            label="Password Confirmation"
+            type={showPasswordConfirmation ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() =>
+                      setShowPasswordConfirmation(!showPasswordConfirmation)
+                    }
+                    edge="end"
+                  >
+                    {showPasswordConfirmation ? (
+                      <VisibilityIcon />
+                    ) : (
+                      <VisibilityOffIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+           
+          >
+            Register
+          </LoadingButton>
+        </Stack>
+      </FormProvider>
+    </Container>
+  );
+};
+
+export default RegisterPage;
