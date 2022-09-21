86e64b82ec2bc066a28eb0e4b528369a4ad1a08d
Kelly <20522051@gm.uit.edu.vn>
2022-09-21 21:51:50 +0700

    Friend, Request, Add Friend

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import AlertMsg from "../components/AlertMsg";
 import MainFooter from "./MainFooter";
 import MainHeader from "./MainHeader";
 
@@ -9,6 +10,7 @@ const MainLayout = () => {
   return (
     <Stack sx={{ minHeight: "100vh" }}>
       <MainHeader />
+      <AlertMsg/>
       <Outlet />
       <Box sx={{flexGrow: 1}}/>
       <MainFooter />
